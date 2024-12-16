ab966de4f029ca1b51e60ffed5d95f86dc4e0c18
Eloy Antoni Pascual Perez-Casaux <epascual@car1s1.42barcelona.com>
2024-12-16 19:40:21 +0100

    Inicio pushswap.

---
+++ psw.c
@@ -0,0 +1,18 @@
+#include "psw.h"
+
+
+/*Parece ser que sa = sb, pa = pb, ra = rb, rra = rrb
+ * Has de utilizar el stack b como si fuera el temporal?
+ * Necesitas una util que cuente el tamano de los arrays
+ * */
+int	*sa(int *a, int *b){}
+int	*sb(int *a, int *b){}
+int	*ss(int *a, int *b){}
+int	*pa(int *a, int *b){}
+int	*pb(int *a, int *b){}
+int	*ra(int *a, int *b){}
+int	*rb(int *a, int *b){}
+int	*rr(int *a, int *b){}
+int	*rra(int *a, int *b){}
+int	*rrb(int *a, int *b){}
+int	*rrr(int *a, int *b){}
