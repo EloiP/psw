1bc83bf0276cef3e441de43f544ca402d276e2e8
Eloy Antoni Pascual Perez-Casaux <epascual@car3s4.42barcelona.com>
2025-01-07 16:53:11 +0100

    Reestructuracion en base a listas.

--- psw.c
+++ psw.c
@@ -1,10 +1,50 @@
-#include "psw.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   psw.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epascual <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 13:39:02 by epascual          #+#    #+#             */
+/*   Updated: 2025/01/07 16:52:02 by epascual         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "include/psw.h"
+#include "include/libft.h"
 
 /*Parece ser que sa = sb, pa = pb, ra = rb, rra = rrb
  * Has de utilizar el stack b como si fuera el temporal?
+ * No usar atoi por si letras intercaladas.
  * Necesitas una util que cuente el tamano de los arrays
  * */
+
+int main(int argc, char *argv[]){
+	t_list **a;
+	t_list **b;
+	int i;
+
+	if (argc < 2)
+		return(1);
+	checker(argc, argv);
+	a = ft_lstnew(NULL);
+	b = ft_lstnew(NULL);
+	i = 1;
+	while (i <= argc)
+	{
+		ft_lstadd_front(a, ft_atoi(*argv[i]));
+	}
+}
+
+int	arraylen(int *a)
+{
+}
+
+int	check(int argc, char **argv)
+{
+	
+}
+//Instrucciones
 int	*sa(int *a, int *b){}
 int	*sb(int *a, int *b){}
 int	*ss(int *a, int *b){}
